55b003cb4c9cbb239d2c4c727a6494f91c478ad1
Ândres Winck Schultz <andreswinck@cwi.com.br>
2021-08-31 22:38:11 -0300

    class 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# apache-camel-alura
+Repository to Alura course about Apache Camel
+
+official Documentation: https://camel.apache.org/components/latest/file-component.html
--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,8 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +10,18 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
 
-	}	
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?delay=5s&noop=true")
+					.log("${id} - ${body}")
+					.log("${exchange.pattern}")
+					.to("file:saida");
+			}
+		});
+
+		context.start();
+		Thread.sleep(2000);
+	}
 }
