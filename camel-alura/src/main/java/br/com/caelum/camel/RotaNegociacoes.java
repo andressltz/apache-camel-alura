e986bae7f3182351ac3f4900d5ee2b868fb67dde
Ândres Winck Schultz <andreswinck@cwi.com.br>
2021-10-05 21:51:20 -0300

    class 3: challenge

--- camel-alura/src/main/java/br/com/caelum/camel/RotaNegociacoes.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaNegociacoes.java
@@ -1,20 +1,27 @@
 package br.com.caelum.camel;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlConnectionPoolDataSource;
 import com.thoughtworks.xstream.XStream;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.xstream.XStreamDataFormat;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.SimpleRegistry;
 
+import java.text.SimpleDateFormat;
 
 public class RotaNegociacoes {
 
 	public static void main(String[] args) throws Exception {
+		SimpleRegistry registry = new SimpleRegistry();
+		registry.put("mysql", criaDataSource());
+
 		final XStream xStream = new XStream();
 		xStream.alias("negociacao", Negociacao.class);
 
-		CamelContext context = new DefaultCamelContext();
+		CamelContext context = new DefaultCamelContext(registry);
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
@@ -25,13 +32,35 @@ public class RotaNegociacoes {
 					.split(body())
 //					.setHeader(Exchange.FILE_NAME, constant("negociacoes.xml"))
 //					.to("file://saida");
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							Negociacao negociacao = exchange.getIn().getBody(Negociacao.class);
+							exchange.setProperty("preco", negociacao.getPreco());
+							exchange.setProperty("quantidade", negociacao.getQuantidade());
+							String data = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss").format(negociacao.getData().getTime());
+							exchange.setProperty("data", data);
+						}
+					})
+					.setBody(simple("insert into negociacao(preco, quantidade, data) values (${property.preco}, ${property.quantidade}, '${property.data}')"))
 					.log("${body}")
-					.end();
+					.delay(1000)
+					.to("jdbc:mysql");
 			}
 		});
 
 		context.start();
 		Thread.sleep(2000);
+	}
+
+	private static MysqlConnectionPoolDataSource criaDataSource() {
+		MysqlConnectionPoolDataSource mysqlDs = new MysqlConnectionPoolDataSource();
+		mysqlDs.setDatabaseName("camel");
+		mysqlDs.setServerName("localhost");
+		mysqlDs.setPort(3306);
+		mysqlDs.setUser("root");
+		mysqlDs.setPassword("bancoroot");
+		return mysqlDs;
 	}
 
 }
