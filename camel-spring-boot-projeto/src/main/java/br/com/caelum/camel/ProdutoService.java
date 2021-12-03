1241ec74ec419e40759d830e882975862e677ca7
Ândres Winck Schultz <andreswinck@cwi.com.br>
2021-12-02 23:42:27 -0300

    class 7: desafio - rota no bean

--- camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/ProdutoService.java
+++ camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/ProdutoService.java
@@ -1,15 +1,15 @@
-package br.com.caelum.camel;
-
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProdutoService extends RouteBuilder {
-
-	@Override
-	public void configure() throws Exception {
-		from("file:pedidos")
-			.to("activemq:queue:pedidos");
-
-	}
-}
+//package br.com.caelum.camel;
+//
+//import org.apache.camel.builder.RouteBuilder;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class ProdutoService extends RouteBuilder {
+//
+//	@Override
+//	public void configure() throws Exception {
+//		from("file:pedidos")
+//			.to("activemq:queue:pedidos");
+//
+//	}
+//}
